1fb6dcc736615c687e3ba17214e280ab9f4d6b13
Matthew Johnson <matthewjohnson@snarkydata.com>
2023-02-02 08:18:19 -0600

    Adding dog.py file

---
+++ Examples/dog.py
@@ -0,0 +1,23 @@
+# dog.py, Chapter 9, Python Crash Course
+# program executes a dog class
+
+class Dog():
+    """a simple attempt to model a dog"""
+
+    def __init__(self, name, age):
+        """initialize name and age attributes"""
+        self.name = name    # attribute
+        self.age = age      # attribute
+
+
+    def sit(self):
+        """simulate a dog sitting in response to a command"""
+        print(self.name.title() + " is now sitting.")
+
+
+    def roll_over(self):
+        """simulate rolling over in response to a command"""
+        print(self.name.title() + " rolled over!")
+
+
+
