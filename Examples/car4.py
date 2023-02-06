1ff7474b6e63d7952b52f154ec06dfe5223b086c
Matthew Johnson <matthewjohnson@snarkydata.com>
2023-02-05 20:48:09 -0600

    Adding car4.py file

---
+++ Examples/car4.py
@@ -0,0 +1,37 @@
+# car4.py, Chapter 9, Python Crash Course
+# program prints information about a car
+
+class Car():
+
+    def __init__(self, make, model, year):
+        """initialize attributes to describe a car"""
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0
+
+
+    def get_descriptive_name(self):
+        """return a neatly formatted descriptive name"""
+        long_name = str(self.year) + ' ' + self.make + ' ' + self.model
+        return long_name.title()
+
+
+    def read_odometer(self):
+        """print a statement showing the car's mileage"""
+        print("This car has " + str(self.odometer_reading) + " miles on it.")
+
+
+    def update_odometer(self, mileage):
+        """set the odometer reading to the given value"""
+        self.odometer_reading = mileage
+
+
+my_new_car = Car('audi', 'a4', 2016)
+print(my_new_car.get_descriptive_name())
+
+my_new_car.update_odometer(23)
+my_new_car.read_odometer()
+
+
+
